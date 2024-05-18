0eb17dc3a07f69e0dd688df15eedacfdde5cc41d
Toby Lad <tobylad@Tobys-MacBook-Air.local>
2024-05-18 12:27:50 -0400

    Remove firebase, modify gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyD-_ABa1Sg_tx2Wo4vMA_uxeFuINRd0vpM",
-    authDomain: "fallout-4-char-builder.firebaseapp.com",
-    projectId: "fallout-4-char-builder",
-    storageBucket: "fallout-4-char-builder.appspot.com",
-    messagingSenderId: "1048070214990",
-    appId: "1:1048070214990:web:ed38062469e4466ed83a1f",
-    measurementId: "G-BBNCJS219D"
-};
-
 const init = (): void => {
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
+    console.log('hey whats up')
 }
 
 document.addEventListener('DOMContentLoaded', () => {
